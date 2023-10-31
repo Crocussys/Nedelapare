b5f93b257caf1ae60e26a7bc7c3c9c7c185b02c1
Crocussys <egor.vladislavovich.nn@gmail.com>
2023-10-31 20:30:18 +0300

    fix models

--- www/www/settings.py
+++ www/www/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Nedelapare_site.apps.NedelapareSiteConfig.'
+    'Nedelapare_site.apps.NedelapareSiteConfig'
 ]
 
 MIDDLEWARE = [
