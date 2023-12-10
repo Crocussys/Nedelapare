5e3c8d2d1c3f2d11bba821ff8e75d1482751c654
Nikita Karetnikov <NikitaKaretnikov@gmail.com>
2023-12-10 16:03:48 +0300

    add interact menu

--- www/static/js/schedule.js
+++ www/static/js/schedule.js
@@ -1,4 +1,4 @@
-var menu_int = document.getElementById("menu_interact")
+var menu_int = document.getElementById('menu_interact')
 
 menu_int.addEventListener('mouseenter', function(){
     var menu = document.querySelector('.menu');
@@ -7,13 +7,20 @@ menu_int.addEventListener('mouseenter', function(){
     backdrop.style.display = 'block';
     var lessons = document.querySelectorAll('.lessons');
     lessons.forEach(function(lesson) {
-    lesson.style.filter = 'blur(5px)';
+        lesson.style.filter = 'blur(4px)';
     });
-    var but = document.querySelector('.menu-button')
-    but.style.width = '300px';
+    var but = document.querySelectorAll('.menu-button')
+    but.forEach(function(btn){
+        btn.style.width = '300px'
+    });
+    var text = document.querySelectorAll(".menu-text")
+    text.forEach(function(txt){
+        txt.style.transition = 'opacity 0.5s'
+        txt.style.opacity = 1;
+    });
 });
 
-menu_int.addEventListener('mouseleave', function(){
+menu_int.addEventListener("mouseleave", function(){
     var menu = document.querySelector('.menu');
     var backdrop = document.querySelector('.backdrop');
     menu.style.width = '60px';
@@ -22,8 +29,15 @@ menu_int.addEventListener('mouseleave', function(){
     lessons.forEach(function(lesson) {
     lesson.style.filter = 'none';
     });
-    var but = document.querySelector('.menu-button')
-    but.style.width = '60px';
+    var but = document.querySelectorAll('.menu-button')
+    but.forEach(function(btn){
+        btn.style.width = '60px';
+    });
+    var text = document.querySelectorAll(".menu-text")
+    text.forEach(function(txt){
+        txt.style.transition = 'none';
+        txt.style.opacity = 0;
+    });
 });
 
 /*function openMenu(){
